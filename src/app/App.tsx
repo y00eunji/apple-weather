a9466a86abdafcf2a664347e09bc401106cf4f96
y00eunji <aine0227@naver.com>
2024-08-29 15:29:50 +0900

    [🏗️]-기본 설계 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,19 @@
+import Daily from '@/widgets/daily';
+import Details from '@/widgets/details';
+import Hourly from '@/widgets/hourly';
+import Radar from '@/widgets/radar';
+import Summary from '@/widgets/summary';
+
 function App() {
   return (
-    <div className="w-full h-dvh flex items-center justify-center">
-      <div className="w-[480px] h-dvh bg-primary opacity-60 flex flex-col items-center gap-5"></div>
+    <div className="w-full h-full flex items-center justify-center text-white">
+      <div className="w-[480px] h-full bg-primary opacity-60 flex flex-col items-center gap-5">
+        <Summary />
+        <Hourly />
+        <Daily />
+        <Radar />
+        <Details />
+      </div>
     </div>
   );
 }
